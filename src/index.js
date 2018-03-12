55364b57874af448f07ea4821e71a7d21af1c0b5
Denis <denisduhnov@mail.ru>
2018-03-12 23:25:53 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 module.exports = function getZerosCount(number, base) {
  
- while (base % 2 == 0 && base != 2) {
+ /*while (base % 2 == 0 && base != 2) {
    base = Math.floor(base / 2);
- };
+ };*/
+
+ var Ans = [];
+ var d = 2;
+   while (d * d <= base) {
+     if (base % d == 0){
+        Ans.unshift(d);
+        base = base/d;
+      }
+       else {d += 1;};
+    }
+   if (base > 1) {
+       Ans.unshift(base);
+   };
+
+function compareReversed(a, b) {
+    return b - a;
+  }
+
+  Ans.sort(compareReversed);
+   
 
  let zeros = 0;
 
  while(number > 0) {
-  number = Math.floor(number / base);
+  number = Math.floor(number / Ans[0]);
   zeros += number;
  };
 
