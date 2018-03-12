9ddc211d0a88e99fbabe60439a1e4f142a5a4d0e
Denis <denisduhnov@mail.ru>
2018-03-12 23:00:33 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+ 
+ while (base % 2 == 0 && base != 2) {
+   base = Math.floor(base / 2);
+ };
+
+ let zeros = 0;
+
+ while(number > 0) {
+  number = Math.floor(number / base);
+  zeros += number;
+ };
+
+ return zeros;
+
 }
